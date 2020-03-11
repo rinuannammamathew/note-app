d1600dad2702daa25ceff4d76a86cc890caeaa7f
Rinu <rinu@Rinus-MacBook-Air.local>
2020-03-10 19:30:57 -0700

    add close icon and edit icon to the note

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class NoteCard extends React.Component{
-  render (){
+class NoteCard extends Component {
+  render() {
+    const { note } = this.props;
+
     return (
-        <div className = "note-card-container">
-          NoteCard Component
+      <div className="note-card-container">
+        <div className="note-card-title">
+         {note.title}
         </div>
+        <div className="note-card-content">
+          {note.content} 
+        </div>
+        <span className="note-card-delete">
+          <i className="material-icons">Close</i>
+        </span>
+        <span className="note-card-edit">
+          <i className="maretial-icons">mode-edit</i>
+        </span>
+      </div>
     );
   }
-
 }
 
 export default NoteCard;
