e8acc49554e7394cae05db54a2146bfd8d5b3581
Rinu <rinu@Rinus-MacBook-Air.local>
2020-03-18 17:57:49 -0700

    coustom trigger inside app component

--- src/App.js
+++ src/App.js
@@ -34,6 +34,10 @@ class App extends Component {
     .then((res) => this.setState({ note: res.data, showNote: true }) )
     .catch((err) => console.log(err.response.data) );
   }
+
+  submitNote = (data) => {
+    console.log(data);
+  }
   render() {
     const { showNote, notes, note } = this.state;
 
@@ -43,6 +47,7 @@ class App extends Component {
             {showNote ?
             <Note
               note={note}
+              submitNote={this.submitNote}
              />
             :
             <List
