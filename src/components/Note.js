309d3adc454fa9a0b7800ce613365d517b22fba5
Rinu <rinu@Rinus-MacBook-Air.local>
2020-03-20 16:09:58 -0700

    add tag button

--- src/components/Note.js
+++ src/components/Note.js
@@ -11,13 +11,21 @@ class Note extends React.Component {
     this.props.submitNote(formData, this.props.note.id);
   }
   renderTagForm() {
-    return (
-      <span>
-        Tag your note:
-        <i className="tag-button material-icons">add circle</i>
-      </span>
-    );
-  };
+    if (!this.props.newTag) {
+      return (
+        <span>
+          Tag your note:
+          <i
+            className="tag-button material-icons"
+            onClick={() => this.props.showTagForm()}
+            >
+            add circle
+          </i>
+        </span>
+      );
+    }
+  }
+  
 
   render() {
     const { note } = this.props;
